fffcb4e7c89340a4c8f3c91dba1500c4e9228da4
Ana Laura Paulino Oliveira <ana.l.oliveira113@aluno.senai.br>
2026-05-29 11:37:49 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = require('./app');
 dotenv.config();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+
+// O listen só deve rodar se NÃO estiver na Vercel (produção)
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+  });
+}
+
+// A linha que a Vercel precisa para funcionar:
+module.exports = app;
